b39cb194153ec4b988528cdf4c1889e7f6808e3f
unknown <arinzeokorji@yahoo.com>
2019-05-05 16:58:02 +0100

    created blog page

--- views/blog.php
+++ views/blog.php
@@ -13,34 +13,141 @@ include 'partials/navbar.php';
 </head>
 <body>
 
-	<div id="wrapper">
+	<!-- Blog title with navigation starts -->
+    <div class="bodyContainer">
+        <div>
+            <h1 id="header">World changing insights, <br>
+            One story at a time.</h1>
+        </div>
 
-		<h3>HNG Blog</h3>
-		<h1><a href="views/admin/login.php">Post to Blog</a></h1>
-		
-		<hr />
+        <!-- Blog navigation starts -->
+        <div class="blogNavSection">
+            <div class="blogNavSectionInner">
+                <ul class="blogNavList">
+                    <li class='active nav-list'><a href="#">All</a></li>
+                    <li class="nav-list"><a href="#">Intern Stories</a></li>
+                    <li class="nav-list"><a href="#">Design</a></li>
+                    <li class="nav-list"><a href="#">Frontend</a></li>
+                    <li class="nav-list"><a href="#">Backend</a></li>
+                    <li class="nav-list"><a href="#">Machine Learning</a></li>
+                    <li class="nav-list"><a href="#">Digital Marketing</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Blog navigation ends -->
+    </div>
 
-		<?php
-			try {
+    <!-- Blog title with navigation ends -->
 
-				$stmt = $db->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
-				while($row = $stmt->fetch()){
-					
-					echo '<div>';
-						echo '<h1><a href="views/viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
-						echo '<p style="color:#990033">Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
-						echo '<p>'.$row['postDesc'].'</p>';				
-						echo '<p><a href="views/viewpost.php?id='.$row['postID'].'">Read More</a></p>';				
-					echo '</div>';
-
-				}
-
-			} catch(PDOException $e) {
-			    echo $e->getMessage();
-			}
-		?>
-
-	</div>
+    <!-- Blog body starts -->
+    <div class="bodyContainer">
+        <div>
+            <div class="blogBodyContainer">
+                    <div class="topPosts">
+                            <div>
+                                <h3 class="topPostsHeader">Top Posts</h3>
+                            </div>
+        
+                            <div class="topPostsBody">
+                                <div class="topPostsArticle">
+                                    <img src="..app/css/img/mask _group.png" alt="Author's Avatar">
+                                    <div class="topPostInfo">
+                                        <h5>20 Relevant Design Trends from 2018</h5>
+                                        <div class="moreBlogInfo">
+                                            <span>by <span class="articleAuthorName"><a href="#">Design Walker</a></span></span>
+                                            <span>2 days ago</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="topPostsArticle">
+                                    <img src="..app/css/img/mask _group.png" alt="Author's Avatar">
+                                    <div class="topPostInfo">
+                                        <h5>20 Relevant Design Trends from 2018</h5>
+                                        <div class="moreBlogInfo">
+                                            <span>by <span class="articleAuthorName"><a href="#">Design Walker</a></span></span>
+                                            <span>2 days ago</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="topPostsArticle">
+                                    <img src="..app/css/img/mask _group.png" alt="Author's Avatar">
+                                    <div class="topPostInfo">
+                                        <h5>20 Relevant Design Trends from 2018</h5>
+                                        <div class="moreBlogInfo">
+                                            <span>by <span class="articleAuthorName"><a href="#">Design Walker</a></span></span>
+                                            <span>2 days ago</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                <div class="blogArticlesList">
+                    <div class="blogArticle">
+                        <div class="imageContainer">
+                                <img src="..app/css/img/mask _group.png" alt="Article's image" srcset="">
+                        </div>
+                        <div class="articleDetails">
+                            <span class="articleCategory">DESIGN</span>
+                            <h5 class="articleTitle">20 Relative Design Trends from 2018</h5>
+                            <p class="articleSummary">They're late. My experiment worked. They're all exactly twenty-five minutes slow. My insurance, it's your car, your insurance should pay for it. Hey, I wanna know ...</p>
+                            <div class="articleInfo">
+                                <img src="..app/css/img/mask _group.png" alt="Author's Avatar" srcset="">
+                                <span>by <span class="articleAuthorName"><a href="#">Design Walker</a> </span> | 4 days ago</span>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="blogArticle">
+                        <div class="imageContainer">
+                                <img src="..app/css/img/mask _group.png" alt="Article's image" srcset="">
+                        </div>
+                        <div class="articleDetails">
+                            <span class="articleCategory">DESIGN</span>
+                            <h5 class="articleTitle">20 Relative Design Trends from 2018</h5>
+                            <p class="articleSummary">They're late. My experiment worked. They're all exactly twenty-five minutes slow. My insurance, it's your car, your insurance should pay for it. Hey, I wanna know ...</p>
+                            <div class="articleInfo">
+                                <img src="..app/css/img/mask _group.png" alt="Author's Avatar" srcset="">
+                                <span>by <span class="articleAuthorName"><a href="#">Design Walker</a> </span> | 4 days ago</span>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="blogArticle">
+                        <div class="imageContainer">
+                                <img src="..app/css/img/mask _group.png" alt="Article's image" srcset="">
+                        </div>
+                        <div class="articleDetails">
+                            <span class="articleCategory">DESIGN</span>
+                            <h5 class="articleTitle">20 Relative Design Trends from 2018</h5>
+                            <p class="articleSummary">They're late. My experiment worked. They're all exactly twenty-five minutes slow. My insurance, it's your car, your insurance should pay for it. Hey, I wanna know ...</p>
+                            <div class="articleInfo">
+                                <img src="..app/css/img/mask _group.png" alt="Author's Avatar" srcset="">
+                                <span>by <span class="articleAuthorName"><a href="#">Design Walker</a> </span> | 4 days ago</span>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="blogArticle">
+                        <div class="imageContainer">
+                                <img src="..app/css/img/mask _group.png" alt="Article's image" srcset="">
+                        </div>
+                        <div class="articleDetails">
+                            <span class="articleCategory">DESIGN</span>
+                            <h5 class="articleTitle">20 Relative Design Trends from 2018</h5>
+                            <p class="articleSummary">They're late. My experiment worked. They're all exactly twenty-five minutes slow. My insurance, it's your car, your insurance should pay for it. Hey, I wanna know ...</p>
+                            <div class="articleInfo">
+                                <img src="/app/css/img/mask _group.png" alt="Author's Avatar" srcset="">
+                                <span>by <span class="articleAuthorName"><a href="#">Design Walker</a> </span> | 4 days ago</span>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <!-- Blog body ends -->  
 	<?php include 'partials/footer.php'?>;
 </body>
 </html>
